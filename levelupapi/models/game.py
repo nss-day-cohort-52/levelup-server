b66081090552668aa25b2844258cd83b36ea7fde
hannahhall <hannahkhall13@gmail.com>
2022-01-31 11:55:35 -0600

    ch 2: models

---
+++ levelupapi/models/game.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Game(models.Model):
+    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    maker = models.CharField(max_length=100)
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    number_of_players = models.IntegerField()
+    skill_level = models.IntegerField()
